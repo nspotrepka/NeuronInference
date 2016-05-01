fb2ae334a854e43c3fe9d1f3c266e9f5d499ed00
Thomas Pinella <txpinella@gmail.com>
2016-05-01 19:35:54 -0400

    all spacing is spaces now :D

--- network.py
+++ network.py
@@ -6,21 +6,20 @@ import neuron
 
 class Network(object):
     def __init__(self, numNeurons):
-    	self.numNeurons = numNeurons
+        self.numNeurons = numNeurons
 
-    	self.neurons = [neuron.Neuron(0, 10, -10, 0.02, 0.01) for i in range(numNeurons)]
-    	self.weights = np.random.randn(numNeurons, numNeurons)
+        self.neurons = [neuron.Neuron(0, 10, -10, 0.02, 0.01) for i in range(numNeurons)]
+        self.weights = np.random.randn(numNeurons, numNeurons)
 
     def update(self, dt):
-    	binaryValues = np.array([n.getBinaryValue() for n in self.neurons])
-    	dv = np.dot(self.weights, binaryValues.transpose())
-    	for i in range(self.numNeurons):
-    		self.neurons[i].update(dv[i], dt)
+        binaryValues = np.array([n.getBinaryValue() for n in self.neurons])
+        dv = np.dot(self.weights, binaryValues.transpose())
+        for i in range(self.numNeurons):
+            self.neurons[i].update(dv[i], dt)
 
 dt = 0.001
 totalTime = 1
 numIterations = int(totalTime/dt)
 network = Network(100)
 for t in range(numIterations):
-	network.update(dt)
-
+    network.update(dt)
