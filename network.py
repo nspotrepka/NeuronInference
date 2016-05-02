9cd5cf7726c306b274ecdb6fe826135a00606e6b
Thomas Pinella <txpinella@gmail.com>
2016-05-02 14:50:21 -0400

    Changed import statement

--- network.py
+++ network.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-import neuron
+from neuron import Neuron
 
 class Network(object):
     def __init__(self, numNeurons):
         self.numNeurons = numNeurons
 
-        self.neurons = [neuron.Neuron(0, 10, -10, 0.02, 0.01) for i in range(numNeurons)]
+        self.neurons = [Neuron(0, 10, -10, 0.02, 0.01) for i in range(numNeurons)]
         self.weights = np.random.randn(numNeurons, numNeurons)
         for i in range(numNeurons):
             self.weights[i,i] = 0
